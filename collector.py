569ddfa652c5eb46d3664985ea6a3a15504ee0c0
cinerin-team <cinerin@mail.com>
2025-06-30 16:21:52 +0200

    using ini for config

--- collector.py
+++ collector.py
@@ -1,7 +1,15 @@
+import configparser
 import csv
 
-from configs import log_folder, output_file
 from functions import process
+
+used_config = 'TC37548.5.6.2.config'
+
+config = configparser.ConfigParser()
+config.read(used_config)
+
+log_folder = config['files']['log_folder']
+output_file = config['files']['output']
 
 with open(log_folder + "/verdict.log", encoding='utf-8') as f:
     lines = f.readlines()
