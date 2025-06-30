656338d6a7d0cce7d51e3ca7bff7646ec1723d70
cinerin-team <cinerin@mail.com>
2025-06-30 17:35:03 +0200

    multiple jobs

--- collector.py
+++ collector.py
@@ -3,15 +3,19 @@ import csv
 
 from functions import process
 
-used_config = 'TC37548.5.6.2.config'
+used_config = 'TC37548.5.6.1-2.config'
 
 config = configparser.ConfigParser()
 config.read(used_config)
 
-log_folder = config['files']['log_folder']
+
 output_file = config['files']['output_file']
 
-results = process(log_folder)
+results = []
+
+log_folders = [d.strip() for d in config['files']['input_dirs'].split(',')]
+for log_folder in log_folders:
+    results = results + process(log_folder)
 
 with open(output_file, "w", newline='', encoding='utf-8') as f:
     writer = csv.writer(f)
